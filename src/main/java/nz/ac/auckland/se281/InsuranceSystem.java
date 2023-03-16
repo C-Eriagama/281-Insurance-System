45cb41769c4187ff115118f635fe4535c1137187
Chanul <ceri930@aucklanduni.ac.nz>
2023-03-16 23:58:00 +1300

    Initialised variables and started providing errors for incorrect input for createNewProfile in task 1

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -5,6 +5,7 @@ import nz.ac.auckland.se281.Main.PolicyType;
 public class InsuranceSystem {
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
+    // ArrayList database = new ArrayList();
   }
 
   public void printDatabase() {
@@ -12,7 +13,21 @@ public class InsuranceSystem {
   }
 
   public void createNewProfile(String userName, String age) {
-    // TODO: Complete this method.
+
+    // Initialise variables to error check and help formatting
+    int nameLength = userName.length();
+    String firstLetter = "" + userName.charAt(0);
+    int years = Integer.parseInt(age);
+
+    userName = firstLetter.toUpperCase() + userName.substring(1, nameLength).toLowerCase();
+
+    if (years < 0) {
+      MessageCli.INVALID_AGE.printMessage(age, userName);
+    }
+
+    if (nameLength < 3) {
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
+    }
   }
 
   public void loadProfile(String userName) {
