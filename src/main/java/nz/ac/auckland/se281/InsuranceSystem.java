d7af78eeac954ccf3d2bf0d1be482932fbd0c4f3
Chanul <ceri930@aucklanduni.ac.nz>
2023-03-17 16:49:09 +1300

    Finished printDatabase and createNewProfile methods

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,15 +1,39 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
+
+  ArrayList<Profile> database = new ArrayList<Profile>();
+
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
-    // ArrayList database = new ArrayList();
   }
 
   public void printDatabase() {
-    // TODO: Complete this method.
+
+    int profileCount = database.size();
+
+    // Print Number of profiles created
+    if (profileCount == 0) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
+      return;
+    } else if (profileCount == 1) {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
+    } else {
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(profileCount), "s", ":");
+    }
+
+    // Print each profile
+    for (int i = 0; i < profileCount; i++) {
+
+      Profile temp = database.get(i);
+      String name = temp.getFirstName();
+      String age = Integer.toString(temp.getAge());
+
+      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(Integer.toString(i + 1), name, age);
+    }
   }
 
   public void createNewProfile(String userName, String age) {
@@ -21,13 +45,34 @@ public class InsuranceSystem {
 
     userName = firstLetter.toUpperCase() + userName.substring(1, nameLength).toLowerCase();
 
+    // Check for any errors with input
+
+    // Check if age is below 0
     if (years < 0) {
       MessageCli.INVALID_AGE.printMessage(age, userName);
+      return;
     }
 
+    // Check if name is too short
     if (nameLength < 3) {
       MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
+      return;
     }
+
+    // Check if username already exists
+    for (int i = 0; i < database.size(); i++) {
+      Profile temp = database.get(i);
+      if (temp.getFirstName() == userName) {
+        MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
+        return;
+      }
+    }
+
+    // Add profile to database
+    database.add(new Profile(userName, years));
+    MessageCli.PROFILE_CREATED.printMessage(userName, age);
+
+    return;
   }
 
   public void loadProfile(String userName) {
--- src/main/java/nz/ac/auckland/se281/Profile.java
+++ src/main/java/nz/ac/auckland/se281/Profile.java
@@ -3,9 +3,11 @@ package nz.ac.auckland.se281;
 
 public class Profile {
 
+  // Instance Fields
   private String name;
   private int age;
 
+  // Profile Constructor
   public Profile(String name, int age) {
     this.name = name;
     this.age = age;
