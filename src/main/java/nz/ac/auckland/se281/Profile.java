e44f9bc99a7165d122f1aace7183e7e33c9a7595
Chanul <ceri930@aucklanduni.ac.nz>
2023-03-21 13:11:55 +1300

    Added a loaded variable to Profile class and adjusted constructor

--- src/main/java/nz/ac/auckland/se281/Profile.java
+++ src/main/java/nz/ac/auckland/se281/Profile.java
@@ -1,16 +1,18 @@
-// Profile class with name and age
 package nz.ac.auckland.se281;
+// Profile class with name and age
 
 public class Profile {
 
   // Instance Fields
   private String name;
   private int age;
+  private boolean loaded;
 
   // Profile Constructor
   public Profile(String name, int age) {
     this.name = name;
     this.age = age;
+    loaded = false;
   }
 
   public String getFirstName() {
