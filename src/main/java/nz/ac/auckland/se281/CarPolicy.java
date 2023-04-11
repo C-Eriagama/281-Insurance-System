04df985e7ca63f61392aaf17ab2655b645f0dc5f
Chanul <ceri930@aucklanduni.ac.nz>
2023-04-11 22:51:39 +1200

    Added Policy classes

---
+++ src/main/java/nz/ac/auckland/se281/CarPolicy.java
@@ -0,0 +1,5 @@
+package nz.ac.auckland.se281;
+
+public class CarPolicy extends Policy {
+  CarPolicy(int sum, String make, String model, String licensePlate, boolean mechanicalBreakdown) {}
+}
---
+++ src/main/java/nz/ac/auckland/se281/HomePolicy.java
@@ -0,0 +1,5 @@
+package nz.ac.auckland.se281;
+
+public class HomePolicy extends Policy {
+  HomePolicy(int sum, String address, boolean rental) {}
+}
